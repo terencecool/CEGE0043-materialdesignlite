6e20e7671f854003e144bd357a8ff10b40b93854
Chao-Hsien Cheng <ucfscde@ucl.ac.uk>
2019-03-10 20:36:43 -0400

    Add track location functionality

---
+++ ucescc6/www/js/userTracking.js
@@ -0,0 +1,14 @@
+function trackLocation(){
+	if(navigator.geolocation){
+		navigator.geolocation.watchPosition(showPosition);
+	}
+	else{
+		document.getElementById('showLocation').innerHTML = "Geolocation is not supported by this browser.";
+	}
+}
+
+function showPosition(position){
+	L.marker([position.coords.latitude,position.coords.longitude]).addTo(mymap).bindPopup("<b>You were here</b>");
+	mymap.setView([position.coords.latitude,position.longitude],13);
+
+}
