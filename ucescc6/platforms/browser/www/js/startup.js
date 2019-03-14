63f95e45d49ac9e297b8f038b68045280db2d452
Chao-Hsien Cheng <ucfscde@ucl.ac.uk>
2019-03-13 20:02:51 -0400

    create automatic startup function

--- ucescc6/platforms/browser/www/js/startup.js
+++ ucescc6/platforms/browser/www/js/startup.js
@@ -2,3 +2,9 @@ function trackAndCircle(){
 	trackLocation()
 	addPointLinePoly()
 }
+
+function startup(){
+	document.addEventListener('DOMContentLoaded', function(){
+		trackAndCircle();
+	},false);
+}
